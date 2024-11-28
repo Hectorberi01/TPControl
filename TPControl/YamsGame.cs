7e2561df85dfafa7e230be425b3e4318be6bfe6b
Hector <hector.adjakpa@gmail.com>
2024-11-28 11:57:56 +0100

    Grande suite
    gestion du test Grande suite

--- TPControl/YamsGame.cs
+++ TPControl/YamsGame.cs
@@ -8,4 +8,12 @@ public class YamsGame
         
         return roll.Distinct().Count() == 1;
     }
+    
+    public bool IsGrandeSuite(List<int> roll)
+    {
+        if (roll.Count != 5) return false;
+        roll.Sort();
+        return roll.SequenceEqual(new List<int> { 1, 2, 3, 4, 5 }) || 
+               roll.SequenceEqual(new List<int> { 2, 3, 4, 5, 6 });
+    }
 }
--- TestYamsGame/UnitTest1.cs
+++ TestYamsGame/UnitTest1.cs
@@ -10,9 +10,28 @@ public class UnitTest1
         // Exemple de lancé de Yams
         List<int> yamsList = new List<int> { 6, 6, 6, 6, 6 };
         List<int> yamsList2 = new List<int> { 6, 6, 6, 6, 5 };
+        List<int> yamsList3 = new List<int> { 2, 3, 4, 5, 6,7};
         var game = new YamsGame();
 
         Assert.True(game.IsYams(yamsList));
         Assert.False(game.IsYams(yamsList2));
+        Assert.False(game.IsYams(yamsList3));
+    }
+
+    [Fact]
+    public void IsGrandeSuite()
+    {
+        List<int> yamsList = new List<int> { 1, 2, 3, 4, 5 };
+        List<int> yamsList2 = new List<int> { 6, 6, 6, 6, 5};
+        List<int> yamsList3 = new List<int> { 2, 3, 4, 5, 6};
+        List<int> yamsList4 = new List<int> { 2, 3, 4, 5, 6,7};
+        
+        var game = new YamsGame();
+        
+        Assert.True(game.IsGrandeSuite(yamsList));
+        Assert.False(game.IsGrandeSuite(yamsList2));
+        Assert.True(game.IsGrandeSuite(yamsList3));
+        Assert.False(game.IsGrandeSuite(yamsList4));
+        
     }
 }
